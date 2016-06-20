69fda7bbe7990218ff726c2d1768142fd3d73954
pawel <pawel@PG>
2016-06-20 10:22:40 +0200

    tests

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+﻿exports.calculateMoney = function (student, adult) {
+    return (student * 2 + adult * 3.8).toFixed(2);
+};
+
+exports.giveMoneyBack = function (all, pay) {
+    return all - pay;
+}
+
+exports.wydaj = function(change, Nominals) {
+    var oldChange = change;
+    var i = 0;
+
+    while (i < Nominals.length) {
+        if (change >= Nominals[i].nominal) {
+
+            P = Math.floor(change / Nominals[i].nominal);
+            if (P <= Nominals[i].count) {
+                change = Math.round(100 * (change - (Nominals[i].nominal * P))) / 100;
+                $.grep(Change, function (e) { return e.nominal == Nominals[i].nominal; })[0].count = P;
+            }
+        }
+        i++;
+    }
+    if (change > 0 && Nominals.length >= 2) {
+        exports.wydaj(oldChange, Nominals.slice(1, Nominals.length))
+    } else if (change == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
---
+++ tests/ticketMachineTests.js
@@ -0,0 +1,32 @@
+﻿var assert = require('assert');
+var converter = require("../scripts/index");
+
+describe('Test Suite 1', function () {
+    it('Test 1', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+
+    it('Test 2', function () {
+        assert.ok(1 === 1, "This shouldn't fail");
+        //assert.ok(false, "This should fail");
+        assert.ok(ax.x() == 2, "oki");
+    })
+});
+
+describe('Index Tests', function () {
+    it('calculateMoney', function () {
+        assert.ok(converter.calculateMoney(0, 0) == 0, "Wrong calculate [0,0]");
+        assert.ok(converter.calculateMoney(1, 0) == 2, "Wrong calculate [1,0]");
+        assert.ok(converter.calculateMoney(0, 1) == 3.8, "Wrong calculate [0,1]");
+        assert.ok(converter.calculateMoney(3, 2) == 13.60, "Wrong calculate [3,2]");
+    });
+    it('giveMoneyBack', function () {
+
+    });
+    it('wydaj', function () {
+        assert.ok(converter.wydaj(15, [15]));
+        assert.ok(converter.wydaj(15, [5, 5, 5]));
+        assert.fail(!converter.wydaj(15, [1, 2, 3]));
+        assert.ok(converter.wydaj(15, [3, 5, 5, 0.5, 0.5, 1]));
+    });
+});
